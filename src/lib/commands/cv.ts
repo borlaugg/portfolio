cd25869c46f9b8e2377681ca40939282d9b98933
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-07-14 01:47:41 +0530

    Update cv.ts

--- src/lib/commands/cv.ts
+++ src/lib/commands/cv.ts
@@ -1,1 +1,8 @@
-
+export default function cv(): { text: string, url: string }[] {
+    return [{
+        text: "Here's a link to my CV",
+        url: "https://borlaugg.github.io/assets/CV.pdf",
+        text: "Here's a link to my resume",
+        url: "https://borlaugg.github.io/assets/CV.pdf"
+    }]
+}
