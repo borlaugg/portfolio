7a5ca58e0737ea1c1fb1fb91d8149d36df56fe99
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-07-14 01:59:38 +0530

    Update social.ts

--- src/lib/commands/social.ts
+++ src/lib/commands/social.ts
@@ -2,15 +2,11 @@ export default function social(): { text: string, url: string}[] {
     return [
         {
             text: 'GitHub',
-            url: 'https://github.com/keshavrathinavel'
+            url: 'https://github.com/borlaugg'
         },
         {
             text: 'LinkedIn',
-            url: 'https://www.linkedin.com/in/keshavrathinavel/'
-        },
-        {
-            text: 'Medium',
-            url: 'https://medium.com/@keshavrathinavel'
+            url: 'https://www.linkedin.com/in/rishabh-ravi-4a3230222'
         }
     ]
 }
