91a06338a4bc6c6a7f388a6341a5d8ab86af3e72
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-07-14 16:17:24 +0530

    Update about.ts

--- src/lib/commands/about.ts
+++ src/lib/commands/about.ts
@@ -1,10 +1,6 @@
 export default function about(): string[] {
     return [
         'Hi there! I\'m Rishabh, an undergraduate studying Electrical Engineering at IIT Bombay.',
-        'Besides my studies, I have a passion for playing football, cooking, and driving.',
-        'I play for my university at the amateur league',
-        'Football gives me an exhilarating rush, cooking allows me to express creativity, '
-        'and driving provides a sense of freedom and adventure.',
-        'I\'m excited to connect and share experiences on and off the field!'
+        ''
     ]
 }
