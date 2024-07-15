68d63f3909c30a05aa86872002e0544d3e524d37
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-07-15 20:11:47 +0530

    Update experience.ts

--- src/lib/commands/experience.ts
+++ src/lib/commands/experience.ts
@@ -1,12 +1,14 @@
 export default function experience(): string[] {
     return [
         'Qualcomm, Hyderabad, India - Modem Systems Engineer',
-        '➢ Revived a GPRS-specific modem firmware and virtual platform for real-world applications and created test case scenarios, emulating L1 commands, to assess and validate functionality',
+        '➢ Revived a GPRS-specific modem firmware and virtual platform for real-world applications and created test case scenarios,',
+        '   emulating L1 commands, to assess and validate functionality',
         '➢ Transitioned the firmware operations from 24 to 32 bits, reducing data conversion time to enhance operation speeds by roughly 2%',
         '➢ Validated the implementation by maintaining signal-to-noise ratio within defined 21 dB thresholds, affirming the accuracy of decoded data',
          ' ', 
         'Nvidia, Bangalore, India - Hardware Intern',
-        '➢ Worked in a team of 5+ to parallelize ANUGA, an open-source hydrodynamic modeling software by profiling and running time analysis of the program on Nvidia Nsight Systems',
+        '➢ Worked in a team of 5+ to parallelize ANUGA, an open-source hydrodynamic modeling software by profiling and running time analysis',
+        '   of the program on Nvidia Nsight Systems',
         '➢ Mentored a batch of 20+ students for two online certification courses on CUDA C & Python'
     ]
 }
