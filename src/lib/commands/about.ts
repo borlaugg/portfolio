084d15bd0c90603fe1dfa2402e05fd61ee093203
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-07-15 09:47:19 +0530

    Update about.ts

--- src/lib/commands/about.ts
+++ src/lib/commands/about.ts
@@ -1,6 +1,5 @@
 export default function about(): string[] {
     return [
-        'Hi there! I\'m Rishabh, an undergraduate studying Electrical Engineering at IIT Bombay.',
-        'I\'m interested in hardware and computer architecture, aspiring to work in the same field.'
+        'Hi there! I\'m Rishabh, an undergraduate studying Electrical Engineering at IIT Bombay.'
     ]
 }
