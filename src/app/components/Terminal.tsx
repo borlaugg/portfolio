830508f339037a9f2b4458d39d0c019ce628706b
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-07-15 20:17:14 +0530

    Update Terminal.tsx

--- src/app/components/Terminal.tsx
+++ src/app/components/Terminal.tsx
@@ -96,7 +96,7 @@ export default function Terminal() {
             <div>
                 {output.map((output, index) => (
                     <div key={index}>
-                        <span className='text-yellow-300 font-semibold'><span
+                        <span className='text-cyan-300 font-semibold'><span
                             className='text-cyan-300'>~</span> rishabh@homepage:home$ <span
                             className='text-white font-normal'>{output.command}</span></span>
                         <div className='px-4 py-2' ref={terminalEndRef}>
@@ -107,7 +107,7 @@ export default function Terminal() {
             </div>
             <div>
                 <form onSubmit={handleSubmit}>
-                    <span className='text-yellow-300 font-semibold'><span className='text-cyan-300'>~</span> rishabh@homepage:home$ </span>
+                    <span className='text-cyan-300 font-semibold'><span className='text-cyan-300'>~</span> rishabh@homepage:home$ </span>
                     <input type='text'
                            className='bg-transparent focus:outline-none text-white cyan-300'
                            value={input}
