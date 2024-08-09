702d2b147ad0c548b2bdcefa3e232f3bb54b6e8d
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-08-09 11:29:27 +0530

    Update experience.ts

--- src/lib/commands/experience.ts
+++ src/lib/commands/experience.ts
@@ -1,9 +1,7 @@
 export default function experience(): string[] {
     return [
         'Qualcomm, Hyderabad, India - Modem Systems Engineer',
-        '➢ Revived a GPRS-specific modem firmware and virtual platform for real-world applications and created test case scenarios,',
-        '   emulating L1 commands, to assess and validate functionality',
-        '➢ Transitioned the firmware operations from 24 to 32 bits, reducing data conversion time to enhance operation speeds by roughly 2%',
+        '➢ Revived a GPRS-specific modem firmware and virtual platform for real-world applications in smart watches',
         '➢ Validated the implementation by maintaining signal-to-noise ratio within defined 21 dB thresholds, affirming the accuracy of decoded data',
          ' ', 
         'Nvidia, Bangalore, India - Hardware Intern',
