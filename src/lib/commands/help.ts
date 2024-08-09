7d955894e1e4c28f7be930b29562a286ac9866f7
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-08-09 12:16:50 +0530

    Update help.ts

--- src/lib/commands/help.ts
+++ src/lib/commands/help.ts
@@ -1,12 +1,13 @@
 export default function help(): string[] {
     return [
-        'help        Display a list of available commands',
-        'about       About me, Rishabh Ravi',
-        'banner      Display the banner again',
-        'cv          View my CVs and resumes',
-        'experience  My professional experience',
-        'hobbies     What I do outside of academia',
-        'social      Find my socials',
-        'clear       Clear the terminal'
+        'help            Display a list of available commands',
+        'about           About me, Rishabh Ravi',
+        'banner          Display the banner again',
+        'cv              View my CVs and resumes',
+        'experience      My professional experience',
+        'publications    List all of my published work',
+        'hobbies         What I do outside of academia',
+        'social          Find my socials',
+        'clear           Clear the terminal'
     ]
 }
