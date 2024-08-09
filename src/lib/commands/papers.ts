a3ddd9cd42ebc10dbe85a7726d3c2b0df00a6796
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-08-09 11:53:30 +0530

    Update papers.ts

--- src/lib/commands/papers.ts
+++ src/lib/commands/papers.ts
@@ -2,7 +2,7 @@ export default function papers(): { text: string, url: string }[] {
     return [{
         text: "K. Rathinavel, I. Kumar, A. Sahoo and R. P. Singh, \"SCAM: Secured Shared Cache Partitioning Scheme,\"",
         url: "",
-        ' ',
+        test:' ',
         text:"Rishabh Ravi, Kalp Vyas, Kaushani Majumdar, Satish Mullleti, \"Subsampling Of Correlated Graph Signals\"",
         url: "",
         
