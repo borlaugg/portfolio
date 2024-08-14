e04b8c158da727cbff109ee623e0cb6b427258e9
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-08-14 14:13:12 +0530

    Update experience.ts

--- src/lib/commands/experience.ts
+++ src/lib/commands/experience.ts
@@ -4,7 +4,7 @@ export default function experience(): string[] {
         '➢ Revived a GPRS-specific modem firmware and virtual platform for real-world applications in smart watches',
         '➢ Validated the implementation by maintaining signal-to-noise ratio within defined 21 dB thresholds, affirming the accuracy of decoded data',
          ' ', 
-        'Nvidia, Bangalore, India - Hardware Intern',
+        'Nvidia, Remote - Hardware Intern',
         '➢ Worked in a team of 5+ to parallelize ANUGA, an open-source hydrodynamic modeling software by profiling and running time analysis',
         '   of the program on Nvidia Nsight Systems',
         '➢ Mentored a batch of 20+ students for two online certification courses on CUDA C & Python'
