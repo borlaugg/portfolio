cae663c36582e8c2aed4f2442fa78eefaeb18320
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-11-04 15:13:15 +0530

    Update papers.ts

--- src/lib/commands/papers.ts
+++ src/lib/commands/papers.ts
@@ -2,8 +2,7 @@ export default function papers(): { text: string, url: string }[] {
     return [{
         text: "Varun Venkitraman, Rishabh Ravi, Tejeshwar Torwade, Nirmal Boran, Virendra Singh \"SCAM: Secured Shared Cache Partitioning Scheme,\"",
         url: ""},
-            {},
-            {text:"Rishabh Ravi, Kalp Vyas, Kaushani Majumdar, Satish Mullleti, \"Subsampling Of Correlated Graph Signals\"",
+            {text:"Ravi, Rishabh, et al. \"Subsampling of Correlated Graph Signals.\" arXiv preprint arXiv:2409.04107 (2024).",
         url: "https://arxiv.org/abs/2409.04107",
         
     }]
