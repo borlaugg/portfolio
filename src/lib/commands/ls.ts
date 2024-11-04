5592952bc838c642b5a128c13491d8f0b6729df4
Rishabh <81488614+borlaugg@users.noreply.github.com>
2024-11-04 15:04:27 +0530

    Update ls.ts

--- src/lib/commands/ls.ts
+++ src/lib/commands/ls.ts
@@ -1,5 +1,17 @@
 export default function ls(): string[] {
     return [
         'You were supposed to type help :/',
+        'Nevertheless, I can provide a response to the following commands',
+        ' ',
+        'help            Display a list of available commands',
+        'about           About me, Rishabh Ravi',
+        'banner          Display the banner again',
+        'cv              View my CVs and resumes',
+        'experience      My professional experience',
+        'projects        A list of my projects',
+        'publications    List all of my published work',
+        'hobbies         What I do outside of academia',
+        'social          Find my socials',
+        'clear           Clear the terminal'
     ]
 }
